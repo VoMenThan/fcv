815c588c0727c118e1aa50e2d7b79a74f2809a6f
VoMenThan <thancn12@gmail.com>
2016-12-15 17:56:22 +0700

    add image & update login

--- login.php
+++ login.php
@@ -1,11 +1,19 @@
 <?php ?>
-/**
- * Created by PhpStorm.
- * User: Vo Men Than
- * Date: 12/15/2016
- * Time: 5:28 PM
- */
-<div id="login">
-    <img src="" alt="">
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: Vo Men Than-->
+<!-- * Date: 12/15/2016-->
+<!-- * Time: 5:28 PM-->
+<!-- */-->
 
+<div id="login">
+    <img src="assets/images/img/logo.png" alt="logo">
+    <div class="namelog"></div>
+    <form action="" id="login" method="">
+        <input type="text" placeholder="Số điện thoại">
+        <input type="text" placeholder="Mật khẩu">
+        <input type="button" value="Đăng Nhập">
+    </form>
+    <div class="forgot">Quên mật khẩu?</div>
+    <div class="register">Đăng ký</div>
 </div>

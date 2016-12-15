fc3a8ef8a0c884ea9207e302d0d8766fe4706ed3
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-15 16:49:03 +0700

    page ctcs

---
+++ CTCS.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+
+<div class="wrap-page text-center ctcs-background">
+    <!-- ?code page -->
+    <p>Cải thiện cuộc sống</p>
+    <img src="assets/images/img/FCV-detail-CTCS.jpg">
+</div>
+
+
+<?php include('footer.php'); ?>

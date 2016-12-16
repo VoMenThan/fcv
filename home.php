ec2fdb9940b3fa0d8162c01d02a3df239177ffb3
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-16 13:22:54 +0700

    a

--- home.php
+++ home.php
@@ -3,10 +3,28 @@
     <!-- ?code page -->
         <div class="background-top"></div>
         <img src="assets/images/img/logo.png">
-        <h1>Chương trình <strong>Tri Ân Khách Hàng</strong></h1>
+        <h1 class="home-title">Chương trình <strong>Tri Ân Khách Hàng</strong></h1>
         <div class="top-item">
             <img src="assets/images/img/FCV-home-traitim.png">
-            <h2>Cả nhà mạnh khỏe, gắn kết yêu thương</h2>
+            <img src="assets/images/img/FCV-canhamanhkhoe.png">
+            <p>Với mong muốn chia sẻ với khách hàng trong việc chăm sóc bản thân và xây dựng gia đình hạnh phúc, FCV mang đến cho khách hàng cơ hội chăm sóc các thành viên trong gia đình mình một cách chu đáo và thiết thực hơn thông qua chương trình tri ân khách hàng <span class="text-primary">Cả nhà mạnh khoẻ - Gắn kết yêu thương"</span></p>
+            <ul class="icon">
+                <li><img src="assets/images/img/icon-one.jpg"></li>
+                <li><img src="assets/images/img/icon-two.jpg"></li>
+                <li><img src="assets/images/img/icon-three.jpg"></li>
+                <li><img src="assets/images/img/icon-four.jpg"></li>
+            </ul>
+        </div>
+        <div class="list-section">
+            <div class="item_section">
+                <img src="assets/images/img/home-img-top.png" class="item_section_background">
+                <img src="assets/images/img/icon-section.png" alt="" class="icon-top">
+
+                <div class="content_section">
+                    <h1>Chăm sóc sức khỏe</h1>
+                </div>
+
+            </div>
         </div>
     </div>
 <?php include('footer.php'); ?>

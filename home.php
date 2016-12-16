2454fbbaf4cd1c269685c7f14855dfa5db3038f8
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-16 15:36:27 +0700

    update home

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <?php include('header.php'); ?>
     <div class="wrap-page text-center home">
-    <!-- ?code page -->
+        <!-- ?code page -->
         <div class="background-top"></div>
         <img src="assets/images/img/logo.png">
-        <h1 class="home-title">Chương trình <strong>Tri Ân Khách Hàng</strong></h1>
+        <h2 class="home-title">Chương trình <strong>Tri Ân Khách Hàng</strong></h2>
         <div class="top-item">
             <img src="assets/images/img/FCV-home-traitim.png">
             <img src="assets/images/img/FCV-canhamanhkhoe.png">
@@ -19,11 +19,81 @@
             <div class="item_section">
                 <img src="assets/images/img/home-img-top.png" class="item_section_background">
                 <img src="assets/images/img/icon-section.png" alt="" class="icon-top">
-
                 <div class="content_section">
                     <h1>Chăm sóc sức khỏe</h1>
+                    <div class="image">
+                        <img src="assets/images/img/FCV-cssk-img.jpg" alt="">
+                    </div>
+                    <p>Giúp cả gia đình bạn yên tâm sức khỏe mà không phải lo lắng vấn đề tài chính</p>
+                    <div class="timeline-section">
+                        <p>Các bệnh viện uy tín rộng khắp</p>
+                        <p>Cơ sở & trang thiết bị hiện đại</p>
+                        <p>Đội ngũ bác sĩ chuyên nghiệp</p>
+                    </div>
+                    <div class="btn-content-section">
+                        <a href="#" class="btn-section dark home"><span>Tìm hiểu thêm</span></a>
+                    </div>
                 </div>
-
+            </div>
+        </div>
+        <div class="list-section home-gkgd">
+            <div class="item_section">
+                <img src="assets/images/img/home-img-top-green.png" class="item_section_background">
+                <img src="assets/images/img/icon-section-two.png" alt="" class="icon-top">
+                <div class="content_section green">
+                    <h1>Gắn kết gia đình</h1>
+                    <div class="image">
+                        <img src="assets/images/img/FCV-gkgd-img.jpg" alt="">
+                    </div>
+                    <p>Đồng hành và hỗ trợ bạn có những khoảnh khoắc tuyệt vời bên cạnh những người bạn yêu thương</p>
+                    <div class="timeline-section">
+                        <p>Dịch vụ du lịch trị giá lên đến 5.000.000 VND</p>
+                        <p>Voucher ăn tối tại các nhà hàng nổi tiếng</p>
+                        <p>Top up điện thoại mệnh giá 100.000 VND</p>
+                    </div>
+                    <div class="btn-content-section">
+                        <a href="#" class="btn-section dark home"><span>Tìm hiểu thêm</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="list-section home-dtcc">
+            <div class="item_section">
+                <img src="assets/images/img/home-img-top-three.png" class="item_section_background">
+                <img src="assets/images/img/icon-section-three.png" alt="" class="icon-top">
+                <div class="content_section gray">
+                    <h1>Đầu tư con cái</h1>
+                    <div class="image">
+                        <img src="assets/images/img/FCV-dtcc-img.jpg" alt="">
+                    </div>
+                    <p>Giúp bạn có thể cung cấp cho con cháu mình sự khởi đầu tốt đẹp nhất</p>
+                    <div class="timeline-section">
+                        <p>Tặng Bảo hiểm Thành tài lập nghiệp phí 1 năm giá 10 triệu</p>
+                        <p>Hỗ trợ các gói học bổng tiếng Anh hấp dẫn</p>
+                    </div>
+                    <div class="btn-content-section">
+                        <a href="#" class="btn-section dark home"><span>Tìm hiểu thêm</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="list-section home-ctcs">
+            <div class="item_section">
+                <img src="assets/images/img/home-img-top-four.png" class="item_section_background">
+                <img src="assets/images/img/icon-section-four.png" alt="" class="icon-top">
+                <div class="content_section ctcs-background">
+                    <h1>Cải thiện cuộc sống</h1>
+                    <div class="image">
+                        <img src="assets/images/img/ctcs_box.png" alt="">
+                    </div>
+                    <p>Cùng bạn nâng cao tiện nghi gia đình, thoả sức mua sắm với các phiếu quà tặng có giá trị hấp dẫn</p>
+                    <div class="timeline-section">
+                        <p>Phiếu quà tặng TNGĐ trị giá lên đến 5.000.000 VND</p>
+                    </div>
+                    <div class="btn-content-section">
+                        <a href="#" class="btn-section dark home"><span>Tìm hiểu thêm</span></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>

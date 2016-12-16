f14f7694b07cd819d0729e7c631a6b55e0efa933
VoMenThan <thancn12@gmail.com>
2016-12-16 11:07:28 +0700

    code ctcs, add hình. xoá hình cũ

--- login.php
+++ login.php
@@ -1,19 +1,27 @@
-<?php ?>
+
 <!--/**-->
 <!-- * Created by PhpStorm.-->
 <!-- * User: Vo Men Than-->
 <!-- * Date: 12/15/2016-->
 <!-- * Time: 5:28 PM-->
 <!-- */-->
-
-<div id="login">
-    <img src="assets/images/img/logo.png" alt="logo">
-    <div class="namelog"></div>
-    <form action="" id="login" method="">
-        <input type="text" placeholder="Số điện thoại">
-        <input type="text" placeholder="Mật khẩu">
-        <input type="button" value="Đăng Nhập">
-    </form>
-    <div class="forgot">Quên mật khẩu?</div>
-    <div class="register">Đăng ký</div>
+<?php include('header.php'); ?>
+<div class="wrap-page text-center login">
+    <!-- ?code page login -->
+    <div id="login" class="content-box">
+        <img src="assets/images/img/logo.png" alt="logo">
+        <div class="name"><span>Đăng Nhập</span></div>
+        <form action="#" id="form_login" method="">
+            <ul>
+                <li><input type="text" size="30" placeholder="Số điện thoại"/></li>
+                <li><input type="text" size="30" placeholder="Mật khẩu" /></li>
+            </ul>
+            <p><input type="submit" value="Đăng nhập" /></p>
+        </form>
+        <div class="forgot"><span>Quên mật khẩu?</span></div>
+    </div>
+    <div id="register" class="foot-box"><span>Đăng ký</span></div>
+    <!-- ?end code page login-->
 </div>
+<?php include('footer.php'); ?>
+
